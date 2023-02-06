42cac72954fc465485ec3e59d2666457373e1187
Shubhangi <shroy@fanatics.com>
2023-02-06 11:42:06 +0530

    The initial change

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// main function
+func main() {
+
+    // Println function is used to
+    // display output in the next line
+    fmt.Println("Enter the first number: ")
+
+    // var then variable name then variable type
+    var first int
+
+    // Taking input from user
+    fmt.Scanln(&first)
+    fmt.Println("Enter the second number: ")
+    var second int
+    fmt.Scanln(&second)
+
+    // Print function is used to
+    // display output in the same line
+    fmt.Print("The result of the addition is: ")
+
+    // Addition of two string
+    fmt.Println(first + second)
+}
